c70e9ae4a895373b01b592dc20361852d7e397f2
Doug <doug@playtfg.com>
2021-11-06 17:35:24 -0500

    aboveBelow Method

--- codingExercise/src/Exercise.java
+++ codingExercise/src/Exercise.java
@@ -1,2 +1,24 @@
+import java.util.HashMap;
+
 public class Exercise {
+
+    public HashMap<String, Integer> aboveBelow(int[] arr, int val){
+        int above = 0;
+        int below = 0;
+
+        for (int num : arr) {
+            if (num > val){
+                above++;
+            }else if (num < val){
+                below++;
+            }
+        }
+
+        HashMap<String, Integer> output = new  HashMap<>();
+
+        output.put("above", above);
+        output.put("bellow", below);
+
+        return output;
+    }
 }
